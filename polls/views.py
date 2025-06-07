b41a0a2342f3adc6e6181fbd17eeb41aaebc3884
정태문 <73479963+Melted-Choco@users.noreply.github.com>
2025-06-07 23:10:11 +0900

    Update polls.views
    Shortcut: Render()

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 
 from .models import Question
 
 # display the latest few questions
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {"latest_question_list": latest_question_list}
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 # displays a question text, with no results but with a form to vote
 def detail(request, question_id):
