1131bd06f55ec65f409e15693f61c506bf6fd90d
정태문 <73479963+Melted-Choco@users.noreply.github.com>
2025-06-19 22:14:31 +0900

    Test for DetailView
    In cases,
    1) future question
    2) past question

--- polls/views.py
+++ polls/views.py
@@ -23,6 +23,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+    
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 # displays results for a particular question
 class ResultsView(generic.DetailView):
