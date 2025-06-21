071f269ca1dfd530addf0f9dee12caee77827396
정태문 <73479963+Melted-Choco@users.noreply.github.com>
2025-06-21 22:23:58 +0900

    Customize the admin form
    Question - setting fieldsets
    Choice - enabled adding Choice objects to a Question.

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+# Customize the admin form
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3 # by default, provide fields for 3 choices
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ["pub_date", "question_text"]
+    
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline] # Choice objects are edited on the Question admin page.
+
+admin.site.register(Question, QuestionAdmin)
